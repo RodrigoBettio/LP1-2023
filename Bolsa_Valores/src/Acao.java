e6aca4aa42cf2ed87cc0cd425bb1ad5119c82e51
RodrigoBettio <125048128+RodrigoBettio@users.noreply.github.com>
2023-10-22 20:04:34 -0300

    Delete Bolsa_Valores directory

--- Bolsa_Valores/src/Acao.java
+++
@@ -1,6 +0,0 @@
-//Código feito por Lucas Milhomem, Pedro Mafra, Rafael Munõz e Rodrigo Bettio
-class Acao extends Ativo {
-    public Acao(String codigo, String nome) {
-        super(codigo, nome);
-    }
-}
--- Bolsa_Valores/src/FII.java
+++
@@ -1,7 +0,0 @@
-//Código feito por Lucas Milhomem, Pedro Mafra, Rafael Munõz e Rodrigo Bettio
-
-class FII extends Ativo {
-    public FII(String codigo, String nome) {
-        super(codigo, nome);
-    }
-}
