ecb48eff824b244284cb85c647325572569d28a9
RodrigoBettio <125048128+RodrigoBettio@users.noreply.github.com>
2023-10-22 21:08:30 -0300

    Add files via upload

---
+++ 4ºBimestre/Agenda 2.0/Agenda/src/AplicativoTarefas.java
@@ -0,0 +1,53 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+class AplicativoTarefas {
+    private List<Usuario> usuarios;
+    private Usuario usuarioLogado;
+    private List<Tarefa> tarefas;
+
+    public AplicativoTarefas() {
+        usuarios = new ArrayList<>();
+        usuarioLogado = null;
+        tarefas = new ArrayList<>();
+    }
+
+    public void adicionarUsuario(Usuario usuario) {
+        usuarios.add(usuario);
+    }
+
+    public boolean autenticarUsuario(String nome, String senha) {
+        for (Usuario usuario : usuarios) {
+            if (usuario.getNome().equals(nome) && usuario.getSenha().equals(senha)) {
+                usuarioLogado = usuario;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void listarTarefas() {
+        System.out.println("Tarefas para " + usuarioLogado.getNome() + ":");
+        for (Tarefa tarefa : tarefas) {
+            System.out.println("Título: " + tarefa.getTitulo());
+            System.out.println("Descrição: " + tarefa.getDescricao());
+            System.out.println("Concluída: " + (tarefa.isConcluida() ? "Sim" : "Não"));
+            System.out.println("---------------");
+        }
+    }
+
+    public void adicionarTarefa(String titulo, String descricao) {
+        Tarefa novaTarefa = new Tarefa(titulo, descricao);
+        tarefas.add(novaTarefa);
+    }
+
+    public void marcarTarefaConcluida(int indice) {
+        if (indice >= 0 && indice < tarefas.size()) {
+            Tarefa tarefa = tarefas.get(indice);
+            tarefa.marcarConcluida();
+        }
+    }
+}
+
+
---
+++ 4ºBimestre/Agenda 2.0/Agenda/src/Tarefa.java
@@ -0,0 +1,31 @@
+class Tarefa {
+    private String titulo;
+    private String descricao;
+    private boolean concluida;
+
+    public Tarefa(String titulo, String descricao) {
+        this.titulo = titulo;
+        this.descricao = descricao;
+        this.concluida = false;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public boolean isConcluida() {
+        return concluida;
+    }
+
+    public void marcarConcluida() {
+        concluida = true;
+    }
+
+    public void desmarcarConcluida() {
+        concluida = false;
+    }
+}
