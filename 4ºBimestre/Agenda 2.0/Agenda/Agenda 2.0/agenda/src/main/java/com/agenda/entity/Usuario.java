c7787345e2c565279b2df5c84ea940950b1acf46
RodrigoBettio <125048128+RodrigoBettio@users.noreply.github.com>
2023-11-22 19:58:03 -0300

    Add files via upload

---
+++ 4ºBimestre/Agenda 2.0/Agenda/Agenda 2.0/agenda/src/main/java/com/agenda/entity/Usuario.java
@@ -0,0 +1,19 @@
+package com.agenda.entity;
+
+class Usuario {
+    private String nome;
+    private String senha;
+
+    public Usuario(String nome, String senha) {
+        this.nome = nome;
+        this.senha = senha;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+}
